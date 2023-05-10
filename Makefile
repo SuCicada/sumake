910700af2a99cd9f86cc59b0fde2173a4dc4e9f6
pengyifu <pengyifu@gmail.com>
2023-05-10 17:44:12 +0900

    optimization

--- Makefile
+++ Makefile
@@ -1,5 +1,25 @@
+.PHONY: test
+
 install:
 	pip install . --verbose
 	ls ~/.sumake
+
 clean:
 	rm -rf bin build sumake.egg-info
+
+publish:
+	python setup.py sdist
+	twine upload dist/* --verbose
+
+	mv ~/.sumake/utils.mk ~/.sumake/utils.mk.bak
+	ln -s $(PWD)/utils.mk ~/.sumake/utils.mk
+dev:
+	$(eval sumake=$(shell command -v sumake))
+	if [ -x "$(sumake)" ]; then rm $(sumake); \
+		else echo $(eval sumake=$(shell conda info --base)/bin/sumake); fi
+	@echo $(sumake)
+	ln -s $(PWD)/bin/sumake $(sumake)
+	chmod +x $(sumake)
+	ls -l $(sumake)
+test:
+	make -f test.mk
--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 import os.path
 import shutil
-
-from setuptools import find_packages
-from setuptools import setup
 import sysconfig
-
-from setuptools.command.develop import develop
-from setuptools.command.install import install
-from wheel.bdist_wheel import bdist_wheel
 from pathlib import Path
 
-# home_dir = os.path.expanduser("~")
+from setuptools import setup
+from wheel.bdist_wheel import bdist_wheel
+
 home_dir = Path.home()
 install_dir = sysconfig.get_paths()['purelib']
 print(home_dir)
@@ -19,51 +14,60 @@ print(install_dir)
 VERSION = "2023.5.9"
 
 current = Path(__file__).parent.absolute()
-sumake_dir = home_dir / ".sumake"
-utils_mk = sumake_dir / "utils.mk"
-bin_dir = current / "bin"
+
+
+def generate_sumake(home_dir, current):
+    sumake_dir = home_dir / ".sumake"
+    bin_dir = current / "bin"
+
+    utils_mk = sumake_dir / "utils.mk"
+
+    if not os.path.exists(sumake_dir):
+        os.makedirs(sumake_dir)
+    if not os.path.exists(bin_dir):
+        os.makedirs(bin_dir)
+    print("current", current)
+
+    shutil.copy(current / "utils.mk", utils_mk)
+    # open("./sumake")
+    with open(current / "./sumake", "r") as template:
+        with open(bin_dir / "sumake", "w") as target:
+            res = template.read().format(
+                UTILS_MK=utils_mk,
+                VERSION=VERSION, )
+            target.write(res)
 
 
 class PreInstallCommand(bdist_wheel):
     def run(self):
-        if not os.path.exists(sumake_dir):
-            os.makedirs(sumake_dir)
-        if not os.path.exists(bin_dir):
-            os.makedirs(bin_dir)
-        print("current", current)
-
-        shutil.copy(current / "utils.mk", utils_mk)
-        with open(bin_dir / "sumake", "w") as f:
-            f.write(f"""
-#!/bin/bash
-make -f {utils_mk} $@
-            """)
+        generate_sumake(home_dir, current)
         bdist_wheel.run(self)
 
 
-setup(
-    author="SuCicada",
-    author_email="pengyifu@gmail.com",
-    classifiers=[],
-    description="A generic makefile for projects.",
-    scripts=["bin/sumake"],
-    cmdclass={
-        # "install": PreInstallCommand,
-        "bdist_wheel": PreInstallCommand
-    },
-    install_requires=["setuptools"],
+if __name__ == '__main__':
+    setup(
+        author="SuCicada",
+        author_email="pengyifu@gmail.com",
+        classifiers=[],
+        description="A generic makefile for projects.",
+        scripts=["bin/sumake"],
+        cmdclass={
+            # "install": PreInstallCommand,
+            "bdist_wheel": PreInstallCommand
+        },
+        install_requires=["setuptools"],
 
-    # include_package_data=True,
-    # install_requires=[],
-    # keywords="",
-    # license="",
-    long_description=open("README.md").read(),
-    name="sumake",
-    # namespace_packages=[],
-    # packages=find_packages(),
-    # py_modules=["."],
-    # test_suite="",
-    url="https://github.com/SuCicada/sumake",
-    version=VERSION,
-    zip_safe=False,
-)
+        # include_package_data=True,
+        # install_requires=[],
+        # keywords="",
+        # license="",
+        long_description=open("README.md").read(),
+        name="sumake",
+        # namespace_packages=[],
+        # packages=find_packages(),
+        # py_modules=["."],
+        # test_suite="",
+        url="https://github.com/SuCicada/sumake",
+        version=VERSION,
+        zip_safe=False,
+    )
